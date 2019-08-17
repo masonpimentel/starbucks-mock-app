e6966cfe59d15bc4ba9b6ce5843345ff83d8dc32
Manolito Mason Pimentel <mason@mpimentel.net>
2019-08-17 10:56:52 -0700

    Simple create user!!

--- app/src/main/java/com/example/starbucks_mock_app/JoinNowActivity.kt
+++ app/src/main/java/com/example/starbucks_mock_app/JoinNowActivity.kt
@@ -2,21 +2,73 @@ package com.example.starbucks_mock_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity;
+import com.amazonaws.auth.AWSCredentialsProvider
+
+import com.amazonaws.mobile.auth.core.IdentityHandler
+import com.amazonaws.mobile.auth.core.IdentityManager
 
 import kotlinx.android.synthetic.main.activity_join_now.*
+import kotlin.concurrent.thread
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.amazonaws.mobile.client.AWSMobileClient
+import com.amazonaws.mobile.config.AWSConfiguration
 
 class JoinNowActivity : AppCompatActivity() {
+    private var dynamoDBMapper: DynamoDBMapper? = null
+    private var credentialsProvider: AWSCredentialsProvider? = null
+    private var awsConfiguration: AWSConfiguration? = null
+
+    companion object {
+        private val TAG: String = this::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_now)
         setSupportActionBar(toolbar)
 
+        AWSMobileClient.getInstance().initialize(this) {
+            credentialsProvider = AWSMobileClient.getInstance().credentialsProvider
+            awsConfiguration = AWSMobileClient.getInstance().configuration
+
+            IdentityManager.getDefaultIdentityManager().getUserID(object : IdentityHandler {
+                override fun handleError(exception: Exception?) {
+                    Log.e(TAG, "Retrieving identity: ${exception?.message}")
+                }
+
+                override fun onIdentityId(identityId: String?) {
+                    Log.d(TAG, "Identity = $identityId")
+                    val cachedIdentityId = IdentityManager.getDefaultIdentityManager().cachedUserID
+                    // Do something with the identity here
+
+                    val client = AmazonDynamoDBClient(AWSMobileClient.getInstance().credentialsProvider)
+                    dynamoDBMapper = DynamoDBMapper.builder()
+                        .dynamoDBClient(client)
+                        .awsConfiguration(AWSMobileClient.getInstance().configuration)
+                        .build()
+                }
+            })
+        }.execute()
     }
 
     fun goToLocator(view: View) {
+        val email = "tuser2@gmail.com"
+        val pass = "tpass2"
+
+        val newUser = UsersDO()
+        newUser.userId = email
+        newUser.email = email
+        newUser.password = pass
+
+        thread(start = true) {
+            dynamoDBMapper?.save(newUser)
+        }
+
         val intent = Intent(this, MapsActivity::class.java)
         startActivity(intent)
     }
---
+++ app/src/main/java/com/example/starbucks_mock_app/UsersDO.java
@@ -0,0 +1,47 @@
+package com.example.starbucks_mock_app;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "starbucksmockapp-mobilehub-1195330471-Users")
+
+public class UsersDO {
+    private String _userId;
+    private String _email;
+    private String _password;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBAttribute(attributeName = "email")
+    public String getEmail() {
+        return _email;
+    }
+
+    public void setEmail(final String _email) {
+        this._email = _email;
+    }
+    @DynamoDBAttribute(attributeName = "password")
+    public String getPassword() {
+        return _password;
+    }
+
+    public void setPassword(final String _password) {
+        this._password = _password;
+    }
+
+}
