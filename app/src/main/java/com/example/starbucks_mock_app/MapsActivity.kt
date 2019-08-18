be164070f11ac205b633e3a773f9551aa4eda2c4
Manolito Mason Pimentel <mason@mpimentel.net>
2019-08-18 15:50:39 -0700

    Add maps page header

--- app/src/main/java/com/example/starbucks_mock_app/MapsActivity.kt
+++ app/src/main/java/com/example/starbucks_mock_app/MapsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.starbucks_mock_app
 
+import android.app.PendingIntent.getActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+//activity was generated during app project creation
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
@@ -17,21 +20,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        supportActionBar?.hide()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -39,5 +34,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val glance = LatLng(49.286580, -123.112429)
         mMap.addMarker(MarkerOptions().position(glance).title("Marker at Glance Vancouver"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(glance, 17.0F))
+    }
+
+    fun popActivity(view: View) {
+        finish()
     }
 }
