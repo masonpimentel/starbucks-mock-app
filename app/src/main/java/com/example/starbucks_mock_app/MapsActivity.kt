790b03c5eb51364871e6b51286e0bcbf3b7ecb7c
Manolito Mason Pimentel <mason@mpimentel.net>
2019-08-18 09:52:08 -0700

    Add Google maps key and pin at Glance

--- app/src/main/java/com/example/starbucks_mock_app/MapsActivity.kt
+++ app/src/main/java/com/example/starbucks_mock_app/MapsActivity.kt
@@ -36,8 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val glance = LatLng(49.286580, -123.112429)
+        mMap.addMarker(MarkerOptions().position(glance).title("Marker at Glance Vancouver"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(glance, 17.0F))
     }
 }
